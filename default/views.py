b005a47f07a9cd4d870855a374bd28674c6ee311
Dino Wei <dreamer@dcsh.tp.edu.tw>
2019-04-10 16:09:19 +0800

    檢視投票主題

--- default/views.py
+++ default/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render_to_response
-from django.views.generic import ListView 
+from django.views.generic import ListView, DetailView
 from .models import *
 
 # Create your views here.
-def poll_list(req):
-    polls = Poll.objects.all()
-    return render_to_response('poll_list.html', {'items': polls})
+#def poll_list(req):
+#    polls = Poll.objects.all()
+#    return render_to_response('poll_list.html', {'items': polls})
 
 class PollList(ListView):
+    model = Poll
+
+class PollDetail(DetailView):
     model = Poll

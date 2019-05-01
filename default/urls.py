f0511b7d03c4ff3a8246843b6166a1aec81f50d7
Dino Wei <dreamer@dcsh.tp.edu.tw>
2019-05-01 15:40:54 +0800

    空路徑重新導向投票主題列表

--- default/urls.py
+++ default/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from django.views.generic import RedirectView
 
 urlpatterns = [
     #path('poll0/', poll_list),
+    path('', RedirectView.as_view(url='poll/')),
     path('poll/', PollList.as_view()),
     path('poll/<int:pk>/', PollDetail.as_view()),
     path('vote/<int:oid>/', PollVote.as_view()),
